9444c28aeede098d31c1af648ca40dbf5a805523
leon <leonfrer@gmail.com>
2022-07-20 17:26:43 +0800

    ch03 done

--- ch031/sec03control_flow/src/main.rs
+++ ch031/sec03control_flow/src/main.rs
@@ -1,8 +1,10 @@
 fn main() {
-    // both of "if arms" be diffrent type
-    // let condition = true;
+    // both of "if arms" be& diffrent type
+    // le&t condition = true;
     // let number = if condition { 1 } else { "two" };
 
+    // Returning value with loop
+    println!("using if in a let Statement");
     let mut counter = 0;
     let result = loop {
         counter += 1;
@@ -11,4 +13,18 @@ fn main() {
         }
     };
     println!("The result is {}", result);
+    println!();
+
+    // for loop
+    println!("for loop");
+    let a = [10, 11, 12, 13, 14];
+    for num in a.iter() {
+        println!("The value is: {}", num);
+    }
+    println!();
+
+    println!("reverse of range");
+    for num in (1..11).rev() {
+        println!("{}!", num);
+    }
 }
