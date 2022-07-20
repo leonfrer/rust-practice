199e917163686b5a827d2f1f97665ab22c83f201
leon <leonfrer@gmail.com>
2022-07-20 18:01:38 +0800

    ch04 start

--- ch04/sec01ownership/src/main.rs
+++ ch04/sec01ownership/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let i1 = 9;
+    let i2 = i1;
+    println!("i1: {}, i2: {}", i1, i2);
+
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("s1 is invalid, s2: {}", s2);
+
+    let s1 = "Hello";
+    let s2 = s1;
+    println!("s1: {}, s2: {}", s1, s2);
+
+    // tuple
+    let t1 = (1, 0.2, "hello", '👌');
+    let t2 = t1;
+    println!("t1.3: {}, t2.3: {}", t1.3, t2.3);
+    let t1 = (1, 0.2, "hello", '👌', String::from("Hello"));
+    let t2 = t1;
+    println!("t1 is invalid, t2.3: {}", t2.3);
+
+    // array
+    let a1 = [1, 2, 3, 4];
+    let a2 = a1;
+    println!("a1[1]: {}, a2[1]: {}", a1[1], a2[1]);
 }
