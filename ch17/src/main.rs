174ba5cfff6879960d349ddf97cb61668fad1dc8
Leon <leonfrer@gmail.com>
2022-10-02 13:41:44 +0800

    17 d

--- ch17/src/main.rs
+++ ch17/src/main.rs
@@ -1,5 +1,17 @@
+mod state_pattern;
 mod trait_object_mod;
 
 fn main() {
-    println!("Hello, world!");
+    use state_pattern::*;
+
+    let mut post: Post = Post::new();
+
+    post.add_text("some text");
+    assert_eq!("", post.content());
+
+    post.request_review();
+    assert_eq!("", post.content());
+
+    post.approve();
+    assert_eq!("some text", post.content());
 }
