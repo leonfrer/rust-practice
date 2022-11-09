b5c5501df37fa68b7053481fdb0b7047c8a976d8
Leon <leonfrer@gmail.com>
2022-11-09 15:05:14 +0800

    finished

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -25,11 +25,6 @@ fn handle_conncetion(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    // let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-    //     ("HTTP/1.1 200 OK", "hello.html")
-    // } else {
-    //     ("HTTP/1.1 404 NOT FOUND", "404.html")
-    // };
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
@@ -42,4 +37,5 @@ fn handle_conncetion(mut stream: TcpStream) {
     let length = contents.len();
     let response = format!("{status_line}\r\nCotent-Length: {length}\r\n\r\n{contents}");
     stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
